86e6e4f15adf9d56907a28d7a6628f60b0824b04
MarsXq <776356314@qq.com>
2018-07-19 09:57:56 +0800

    md

---
+++ app/src/main/java/com/xq/mystyle/MyApplication.java
@@ -0,0 +1,14 @@
+package com.xq.mystyle;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+public class MyApplication extends Application {
+
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+    }
+
+}
